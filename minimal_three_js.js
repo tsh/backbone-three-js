a1d811f06984554134653144026ee56d3debee7e
Anton Nikulin <dr.tallin@gmail.com>
2015-04-12 21:33:47 +0300

    red cube on the plane

--- minimal_three_js.js
+++ minimal_three_js.js
@@ -7,14 +7,41 @@ function init(){
 	
 	renderer = new THREE.WebGLRenderer();
 	renderer.setClearColor(0x000000, 1.0);
-	renderer.setSize(window.innerWidth, window.innerHeight); // or window.innerWidth/Height
+	renderer.setSize(window.innerWidth, window.innerHeight); 
 	renderer.shadowMapEnabled = true;
+	
+	// elements of the scene
+	// Plane
+	var planeGeometry = new THREE.PlaneGeometry(20, 20);
+	var planeMaterial = new THREE.MeshLambertMaterial({color: 0xcccccc});
+	var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+	plane.receiveShadow = true;
+	// by default plane is vertical; rotate to horizontal. 90 deg.
+	plane.rotation.x = -0.5 * Math.PI;
+	plane.position.x = 0;
+	plane.position.y = -2;
+	plane.position.z = 0;
+	scene.add(plane);
+	
+	// Cube
+	var cubeGeometry = new THREE.BoxGeometry(6, 4, 6);
+	var cubeMaterial = new THREE.MeshLambertMaterial({color: 'red'});
+	var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+	cube.castShadow = true;
+	scene.add(cube);
 	
 	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 	camera.position.x = 15;
 	camera.position.y = 16;
 	camera.position.z = 13;
 	camera.lookAt(scene.position);
+	
+	var spotLight = new THREE.SpotLight(0xffffff);
+	spotLight.position.set(10, 20, 20);
+	spotLight.shadowCameraNear = 20;
+	spotLight.shadowCameraFar = 50;
+	spotLight.castShadow = true;
+	scene.add(spotLight);
 	
 	document.body.appendChild(renderer.domElement);
 	
